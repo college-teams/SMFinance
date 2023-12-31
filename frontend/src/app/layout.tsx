d48cec03551e0f6f46fe0c5a947b75db847e6ade
Rahul krishnan <rahulkrishnanr016@gmail.com>
2023-12-31 10:33:55 +0530

    UI setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "SM finance",
+  description: "This is finance emi manage app",
+  icons: {
+    icon: [
+      {
+        url: "/images/icon.jpeg",
+        href: "/images/icon.jpeg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        {children}
+        </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="relative">
+      hello world
+    </main>
+  );
+}
