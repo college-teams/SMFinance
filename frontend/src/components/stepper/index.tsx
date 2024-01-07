88cd3cde936119063888692e120ad60833c006bb
Rahul krishnan <rahulkrishnanr016@gmail.com>
2024-01-07 14:02:53 +0530

    stepper logic added

---
+++ frontend/src/components/stepper/index.tsx
@@ -0,0 +1,266 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CustomerInfoForm from "../CustomerInfo";
+import DocumentUploadForm from "../DocumentUpload";
+import { StepConnector, styled } from "@mui/material";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import { IoMdSettings } from "react-icons/io";
+import { IoDocumentAttachSharp } from "react-icons/io5";
+import { stepConnectorClasses } from "@mui/material/StepConnector";
+import { StepIconProps } from "@mui/material/StepIcon";
+import { useState } from "react";
+
+const steps = ["Customer Information", "Document Upload"];
+
+export default function HorizontalLinearStepper() {
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set<number>());
+
+  const isStepOptional = (step: number) => {
+    return false; // No optional steps for simplicity
+  };
+
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setSkipped(new Set<number>());
+  };
+
+  const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 22,
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        backgroundImage:
+          "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        backgroundImage:
+          "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      height: 3,
+      border: 0,
+      backgroundColor:
+        theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
+      borderRadius: 1,
+    },
+  }));
+
+  const ColorlibStepIconRoot = styled("div")<{
+    ownerState: { completed?: boolean; active?: boolean };
+  }>(({ theme, ownerState }) => ({
+    backgroundColor:
+      theme.palette.mode === "dark" ? theme.palette.grey[700] : "#ccc",
+    zIndex: 1,
+    color: "#fff",
+    width: 50,
+    height: 50,
+    display: "flex",
+    borderRadius: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    ...(ownerState.active && {
+      backgroundImage:
+        "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+      boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
+    }),
+    ...(ownerState.completed && {
+      backgroundImage:
+        "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+    }),
+  }));
+
+  function ColorlibStepIcon(props: StepIconProps) {
+    const { active, completed, className } = props;
+
+    const icons: { [index: string]: React.ReactElement } = {
+      1: <IoMdSettings />,
+      2: <IoDocumentAttachSharp />,
+    };
+
+    return (
+      <ColorlibStepIconRoot
+        ownerState={{ completed, active }}
+        className={className}
+      >
+        {icons[String(props.icon)]}
+      </ColorlibStepIconRoot>
+    );
+  }
+
+  const getStepContent = (step: number) => {
+    switch (step) {
+      case 0:
+        return <CustomerInfoForm onNext={handleNext} />;
+      case 1:
+        return <DocumentUploadForm onBack={handleBack} onNext={handleNext} />;
+      default:
+        return "Unknown step";
+    }
+  };
+
+  return (
+    // <Box width={"100%"}>
+
+    <>
+      <div className="w-full">
+        <Stepper
+          className="relative text-white w-full"
+          alternativeLabel
+          activeStep={activeStep}
+          connector={<ColorlibConnector />}
+        >
+          {steps.map((label) => (
+            <Step className="relative text-white" key={label}>
+              <StepLabel
+                className="relative text-white"
+                StepIconComponent={ColorlibStepIcon}
+              >
+                <span className="relative text-white">{label}</span>
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </div>
+
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              {"All steps completed - you're finished"}
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Box sx={{ flex: "1 1 auto" }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </div>
+        ) : (
+          <div>
+            {activeStep === steps.length ? (
+              <div>
+                <Typography sx={{ mt: 2, mb: 1 }}>
+                  {"All steps completed - you're finished"}
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                  <Box sx={{ flex: "1 1 auto" }} />
+                  <Button onClick={handleReset}>Reset</Button>
+                </Box>
+              </div>
+            ) : (
+              <div>{getStepContent(activeStep)}</div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+
+    // </Box>
+  );
+}
+
+// import Box from "@mui/material/Box";
+// import Stepper from "@mui/material/Stepper";
+// import Step from "@mui/material/Step";
+// import StepLabel from "@mui/material/StepLabel";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+// import { useState } from "react";
+// import CustomerInfoForm from "../CustomerInfo";
+// import DocumentUploadForm from "../DocumentUpload";
+
+// const steps = ["Customer Information", "Document Upload"];
+
+// export default function HorizontalLinearStepper() {
+//   const [activeStep, setActiveStep] = useState(0);
+//   const [skipped, setSkipped] = useState(new Set<number>());
+
+//   const isStepOptional = (step: number) => {
+//     return false; // No optional steps for simplicity
+//   };
+
+//   const isStepSkipped = (step: number) => {
+//     return skipped.has(step);
+//   };
+
+//   const handleNext = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+//   };
+
+//   const handleBack = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+//   };
+
+//   const handleReset = () => {
+//     setActiveStep(0);
+//     setSkipped(new Set<number>());
+//   };
+
+//   const getStepContent = (step: number) => {
+//     switch (step) {
+//       case 0:
+//         return <CustomerInfoForm onNext={handleNext} />;
+//       case 1:
+//         return <DocumentUploadForm onBack={handleBack} onNext={handleNext} />;
+//       default:
+//         return "Unknown step";
+//     }
+//   };
+
+//   return (
+//     <Box sx={{ width: "100%" }}>
+//       <Stepper activeStep={activeStep} alternativeLabel>
+//         {steps.map((label, index) => {
+//           const stepProps: { completed?: boolean } = {};
+//           const labelProps: { optional?: React.ReactNode } = {};
+//           if (isStepOptional(index)) {
+//             labelProps.optional = (
+//               <Typography variant="caption">Optional</Typography>
+//             );
+//           }
+//           if (isStepSkipped(index)) {
+//             stepProps.completed = false;
+//           }
+//           return (
+//             <Step key={label} {...stepProps}>
+//               <StepLabel {...labelProps}>{label}</StepLabel>
+//             </Step>
+//           );
+//         })}
+//       </Stepper>
+//       <div>
+//         {activeStep === steps.length ? (
+//           <div>
+//             <Typography sx={{ mt: 2, mb: 1 }}>
+//               {"All steps completed - you're finished"}
+//             </Typography>
+//             <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+//               <Box sx={{ flex: "1 1 auto" }} />
+//               <Button onClick={handleReset}>Reset</Button>
+//             </Box>
+//           </div>
+//         ) : (
+//           <div>{getStepContent(activeStep)}</div>
+//         )}
+//       </div>
+//     </Box>
+//   );
+// }
